154b7d83c9abf6105bad336871ca7bd1c98306c6
Sahe <saheththiyans@gmail.com>
2026-02-03 21:56:27 +0530

    Loaded env variables in db.ts

--- backend/src/service/db.ts
+++ backend/src/service/db.ts
@@ -1,6 +1,8 @@
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../generated/prisma/client.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({ connectionString });

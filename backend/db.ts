dea44d10723b99df7f893a9b26f1f80c369b9e08
YohanJaya <nyajayasinghedmc10@gmail.com>
2026-02-03 22:53:08 +0530

    Initial codes for the backend

--- backend/db.ts
+++ backend/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../../generated/prisma/client.js';
+import { PrismaClient } from './generated/prisma/client.js';
 
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({ connectionString });
--- backend/src/service/clinicService.ts
+++ backend/src/service/clinicService.ts
@@ -1,4 +1,4 @@
-import { prisma } from './db.js';
+import { prisma } from '../../db.js';
 
 export const getAllClinics = async () => {
   return prisma.clinic.findMany();
